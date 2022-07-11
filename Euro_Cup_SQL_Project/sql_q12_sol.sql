93922b48c4fc4a25a9d1fff28755b40f67fea5db
shrirupdwivedi <68109182+shrirupdwivedi@users.noreply.github.com>
2022-07-11 14:00:18 -0400

    Add files via upload

--- Euro_Cup_SQL_Project/sql_q12_sol.sql
+++ Euro_Cup_SQL_Project/sql_q12_sol.sql
@@ -1,2 +1,10 @@
-SELECT team_id ,COUNT(*) FROM goal_details 
-GROUP BY team_id 
+Select pp.position_desc, sc.country_name , count(*) as count 
+from goal_details gd, 
+	 player_mast pm,
+     playing_position pp,
+     soccer_country sc 
+where gd.player_id = pm.player_id
+  and pm.posi_to_play = pp.position_id
+  and gd.team_id = sc.country_id 
+group by pp.position_desc, sc.country_name 
+order by pp.position_desc, sc.country_name;
