418a53f301720a915cf76731534287c436324c69
shrirupdwivedi <68109182+shrirupdwivedi@users.noreply.github.com>
2022-07-11 13:59:38 -0400

    Add files via upload

--- Euro_Cup_SQL_Project/sql_q17_sol.sql
+++ Euro_Cup_SQL_Project/sql_q17_sol.sql
@@ -1,2 +1,13 @@
-SELECT MAX(country_id) 'Country with most assistant referee', COUNT(ass_ref_id) as 'total nummber of assistant referee' 
-FROM asst_referee_mast 
+
+SELECT 
+	sc.country_name
+	,count(*) AS 'ass_ref_count'
+FROM asst_referee_mast AS arm
+
+INNER JOIN soccer_country AS sc ON
+	arm.country_id = sc.country_id
+GROUP BY
+	sc.country_name
+ORDER BY 
+	ass_ref_count DESC
+LIMIT 1
