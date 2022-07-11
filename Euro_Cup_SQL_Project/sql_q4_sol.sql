ad276f3569e4c6ff57e1f0fa9a8d020e9c3442a3
shrirupdwivedi <68109182+shrirupdwivedi@users.noreply.github.com>
2022-07-11 14:01:13 -0400

    Add files via upload

--- Euro_Cup_SQL_Project/sql_q4_sol.sql
+++ Euro_Cup_SQL_Project/sql_q4_sol.sql
@@ -2,6 +2,7 @@ select m.play_stage, COUNT(*) as 'number of substitution'
 from player_in_out as p 
 INNER JOIN match_mast as m 
 USING(match_no) 
+WHERE in_out ='I'
 GROUP BY (m.play_stage) 
 
 -- sql_q1_sol
--- Euro_Cup_SQL_Project/sql_q6_sol.sql
+++ Euro_Cup_SQL_Project/sql_q6_sol.sql
@@ -1,5 +1,11 @@
-SELECT COUNT(*) 
-FROM match_details 
-WHERE win_lose ='W' and decided_by = 'N' and goal_score = 1 
+
+SELECT COUNT(*)
+FROM euro_cup_2016.match_details AS t1, euro_cup_2016.match_details AS t2
+WHERE t1.match_no = t2.match_no
+AND t1.team_id != t2.team_id
+AND t1.goal_score - t2.goal_score = 1
+AND t1.win_lose = 'W'
+AND t1.penalty_score IS NULL
 
 -- sql_q1_sol
+
